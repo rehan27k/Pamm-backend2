7ffdc4e15c98dc6f72e06d95a1dc48aae71c34b0
rehan27k <rehansworld989@gmail.com>
2025-09-15 00:06:12 +0530

    Create user.js

---
+++ src/routes/user.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const User = require("../models/User");
+
+const router = express.Router();
+
+// Test route
+router.get("/test", (req, res) => {
+  res.json({ message: "Users route is working!" });
+});
+
+// Add user
+router.post("/", async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+    const newUser = new User({ name, email, password, role });
+    await newUser.save();
+    res.json(newUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Get all users
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+module.exports = router;
