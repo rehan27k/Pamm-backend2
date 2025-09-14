ee83b0fbb3a73e0f024c4738937e72a52f662a86
rehan27k <rehansworld989@gmail.com>
2025-09-15 00:04:02 +0530

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,14 +1,13 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ["user", "admin"], default: "user" }
+    role: { type: String, enum: ["user", "admin"], default: "user" },
   },
   { timestamps: true }
 );
 
-export default mongoose.model("User", userSchema);
-
+module.exports = mongoose.model("User", userSchema);
